5e6fa9b573ecd26ad12884b66528df7c3b903e06
USER <USER@DESKTOP-KDQ7DFE>
2025-06-19 18:46:00 +0300

    Clean

--- include/Systems/CollisionDetector.h
+++ include/Systems/CollisionDetector.h
@@ -2,7 +2,6 @@
 
 #include "Entity.h"
 #include <cmath>
-#include <algorithm>
 
 namespace FishGame
 {
@@ -19,42 +18,12 @@ namespace FishGame
             return EntityUtils::areColliding(entity1, entity2);
         }
 
-        // Check if a point is inside a circle
-        static bool pointInCircle(const sf::Vector2f& point, const sf::Vector2f& circleCenter, float radius)
-        {
-            float dx = point.x - circleCenter.x;
-            float dy = point.y - circleCenter.y;
-            float distanceSquared = dx * dx + dy * dy;
-
-            return distanceSquared < (radius * radius);
-        }
-
-        // Check collision between two rectangles
-        static bool checkRectangleCollision(const sf::FloatRect& rect1, const sf::FloatRect& rect2)
-        {
-            return rect1.intersects(rect2);
-        }
-
         // Get distance between two points
         static float getDistance(const sf::Vector2f& point1, const sf::Vector2f& point2)
         {
             float dx = point1.x - point2.x;
             float dy = point1.y - point2.y;
             return std::sqrt(dx * dx + dy * dy);
-        }
-
-        // Get distance between two entities
-        static float getDistance(const Entity& entity1, const Entity& entity2)
-        {
-            return EntityUtils::distance(entity1, entity2);
-        }
-
-        // Get squared distance between two points (more efficient when exact distance not needed)
-        static float getDistanceSquared(const sf::Vector2f& point1, const sf::Vector2f& point2)
-        {
-            float dx = point1.x - point2.x;
-            float dy = point1.y - point2.y;
-            return dx * dx + dy * dy;
         }
     };
 }
