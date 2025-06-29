6ece65d1cbf05b06213e08d221a5a72c50faf50a
Abed El Rahman Khalil <abed3.khalil168@gmail.com>
2025-06-29 22:43:34 +0300

    Add volume controls and UI updates to GameOptions

--- include/Core/MusicPlayer.h
+++ include/Core/MusicPlayer.h
@@ -1,50 +1,51 @@
 #pragma once
 
 #include <SFML/Audio.hpp>
-#include <unordered_map>
 #include <string>
 #include <thread>
+#include <unordered_map>
 
 namespace FishGame {
 
 enum class MusicID {
-    MenuTheme,
-    InGame1,
-    InGame2,
-    InGame3,
-    BonusStage,
-    InstructionsHelp,
-    ScoreSummary,
-    StageCleared,
-    PlayerDies
+  MenuTheme,
+  InGame1,
+  InGame2,
+  InGame3,
+  BonusStage,
+  InstructionsHelp,
+  ScoreSummary,
+  StageCleared,
+  PlayerDies
 };
 
 class MusicPlayer {
 public:
-    MusicPlayer();
+  MusicPlayer();
 
-    void play(MusicID theme, bool loop = true);
-    void stop();
-    void setVolume(float volume);
+  void play(MusicID theme, bool loop = true);
+  void stop();
+  void setVolume(float volume);
+  float getVolume() const { return m_volume; }
 
-    static constexpr float FadeDuration = 0.5f; ///< seconds
+  static constexpr float FadeDuration = 0.5f; ///< seconds
 
 private:
-    using MusicPtr = std::unique_ptr<sf::Music>;
+  using MusicPtr = std::unique_ptr<sf::Music>;
 
-    /// Pre-loaded music tracks for fast playback
-    std::unordered_map<MusicID, MusicPtr> m_musicTracks;
+  /// Pre-loaded music tracks for fast playback
+  std::unordered_map<MusicID, MusicPtr> m_musicTracks;
 
-    /// Mapping from music ID to file name
-    std::unordered_map<MusicID, std::string> m_filenames;
+  /// Mapping from music ID to file name
+  std::unordered_map<MusicID, std::string> m_filenames;
 
-    /// Currently playing music (non-owning pointer)
-    sf::Music* m_current;
+  /// Currently playing music (non-owning pointer)
+  sf::Music *m_current;
 
-    /// Background cross fade thread
-    std::jthread m_fadeThread;
+  /// Background cross fade thread
+  std::jthread m_fadeThread;
 
-    float m_volume;
+  float m_volume;
 };
 
-}
+} // namespace FishGame
--- include/Core/SoundPlayer.h
+++ include/Core/SoundPlayer.h
@@ -1,52 +1,52 @@
 #pragma once
 
 #include <SFML/Audio.hpp>
-#include <unordered_map>
-#include <vector>
 #include <memory>
 #include <string>
+#include <unordered_map>
+#include <vector>
 
 namespace FishGame {
 
 enum class SoundEffectID {
-    Bite1,
-    Bite2,
-    Bite3,
-    Bite4,
-    FreezePowerup,
-    LifePowerup,
-    MineExplode,
-    MouseDown,
-    MouseOver,
-    OysterPearl,
-    PlayerGrow,
-    PlayerPoison,
-    PlayerSpawn,
-    PlayerStunned,
-    PufferBounce,
-    SpeedEnd,
-    SpeedStart,
-    StageIntro,
-    StarPickup,
-    FeedingFrenzy,
-    SuperFrenzy
+  Bite1,
+  Bite2,
+  Bite3,
+  Bite4,
+  FreezePowerup,
+  LifePowerup,
+  MineExplode,
+  MouseDown,
+  MouseOver,
+  OysterPearl,
+  PlayerGrow,
+  PlayerPoison,
+  PlayerSpawn,
+  PlayerStunned,
+  PufferBounce,
+  SpeedEnd,
+  SpeedStart,
+  StageIntro,
+  StarPickup,
+  FeedingFrenzy,
+  SuperFrenzy
 };
 
 class SoundPlayer {
 public:
-    SoundPlayer();
+  SoundPlayer();
 
-    void play(SoundEffectID effect);
-    void setVolume(float volume);
+  void play(SoundEffectID effect);
+  void setVolume(float volume);
+  float getVolume() const { return m_volume; }
 
 private:
-    using BufferPtr = std::unique_ptr<sf::SoundBuffer>;
+  using BufferPtr = std::unique_ptr<sf::SoundBuffer>;
 
-    std::unordered_map<SoundEffectID, BufferPtr> m_soundBuffers;
-    std::unordered_map<SoundEffectID, std::string> m_filenames;
-    std::vector<sf::Sound> m_sounds;
-    float m_volume;
+  std::unordered_map<SoundEffectID, BufferPtr> m_soundBuffers;
+  std::unordered_map<SoundEffectID, std::string> m_filenames;
+  std::vector<sf::Sound> m_sounds;
+  float m_volume;
 };
 
-}
-
+} // namespace FishGame
