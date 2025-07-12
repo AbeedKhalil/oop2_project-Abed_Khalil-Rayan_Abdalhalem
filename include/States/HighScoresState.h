caf123eec3c2510cbeb9e854c4e671e5a4b39c21
Abed El Rahman Khalil <abed3.khalil168@gmail.com>
2025-07-12 13:16:33 +0300

    Use unordered_set to maintain unique high score names

--- include/States/HighScoresState.h
+++ include/States/HighScoresState.h
@@ -3,10 +3,23 @@
 #include "GameConstants.h"
 #include "Utils/HighScoreIO.h"
 #include <vector>
+#include <unordered_set>
 
 namespace FishGame {
 class HighScoresState;
 template<> struct is_state<HighScoresState> : std::true_type {};
+
+struct HighScoreEntryHash {
+    std::size_t operator()(const HighScoreEntry& e) const noexcept {
+        return std::hash<std::string>{}(e.name);
+    }
+};
+
+struct HighScoreEntryEqual {
+    bool operator()(const HighScoreEntry& a, const HighScoreEntry& b) const noexcept {
+        return a.name == b.name;
+    }
+};
 
 class HighScoresState : public State {
 public:
@@ -20,6 +33,7 @@ public:
 
 private:
     void loadScores();
+    std::unordered_set<HighScoreEntry, HighScoreEntryHash, HighScoreEntryEqual> m_scoreSet;
     std::vector<HighScoreEntry> m_scores;
     sf::Text m_titleText;
     std::vector<sf::Text> m_scoreTexts;
